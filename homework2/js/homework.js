d6658bd2fcd2dc83a0bf2ab82dedc990f9415881
Стас Шилов <shilowstanislaw@gmail.com>
2021-04-10 03:55:18 +0300

    master refactered

--- homework2/js/homework.js
+++ homework2/js/homework.js
@@ -41,11 +41,56 @@ if(random>25 & random<200) {
    console.log ("Число не содержится в интервале (25;200)");
 }
 
-let random = Math.floor(Math.random(10, 500));
+let random = Math.floor(Math.random() * (500 - 10)) + 10;
 
 if(random>25 & random<200) {
    console.log ("Число содержится в интервале (25;200)");
 
 } else if (random<25 & random>200){
    console.log ("Число не содержится в интервале (25;200)");
+}
+//4
+for (let i=2; i+=2) {//переменная старта i=2
+   console.log(i);
+}
+//5
+for (let i=0; n=1; i<=10; i+=n,n+=i) {
+
+    console.log(i,n)//???
+ }
+
+ //6
+ for (let i=1, n=1; i<7, i+=1) {
+    console.log(i * n);
+ }
+//7
+let userNum;
+
+while(true){
+  //ввести число
+  let userNum=parseFloat(prompt("Введите число"))
+    if(num>50){
+    console.log("Загаданное число больше 50?")
+  }else if(userNum=0)
+      break;
+  }
+    if(num<50){
+      console.log("Загаданное число меньше 50?");
+   }else if(userNum=1)
+      break;
+
+    if(num>25){
+       console.log("Загаданное число больше 25?");
+  }else if(userNum=1)
+      break;
+    if(num>33){
+    console.log("Загаданное число больше 33?");
+  }else if(userNum=1) {
+    console.log("Загаданное число больше 40?")
+  }else if(userNum=0) {
+      console.log("Загаданное число больше 37?")
+  }else if(userNum=1) {
+        console.log("Загаданное число больше 38?")
+  }
+      break;
 }
